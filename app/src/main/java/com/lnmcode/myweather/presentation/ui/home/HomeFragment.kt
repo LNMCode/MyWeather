433818396a13db3aa9ecf27cb8a3dba2719555dd
LNMCode <longnmp31@gmail.com>
2022-06-04 13:47:05 +0700

    Init view home fragment

---
+++ app/src/main/java/com/lnmcode/myweather/presentation/ui/home/HomeFragment.kt
@@ -0,0 +1,22 @@
+package com.lnmcode.myweather.presentation.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lnmcode.myweather.R
+import com.lnmcode.myweather.databinding.FragmentHomeBinding
+import com.lnmcode.myweather.presentation.base.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+    override fun setUpLayout() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setUpEvents() {
+        TODO("Not yet implemented")
+    }
+
+}
