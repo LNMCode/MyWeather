7fe8b2365a5eb4bd42d0acfd72d6ecdbfe0db0ac
LNMCode <longnmp31@gmail.com>
2022-06-04 21:26:08 +0700

    update weather converter into moshi converter

--- app/src/main/java/com/lnmcode/myweather/datasource/cache/converters/WeatherConverter.kt
+++ app/src/main/java/com/lnmcode/myweather/datasource/cache/converters/WeatherConverter.kt
@@ -1,16 +1,18 @@
 package com.lnmcode.myweather.datasource.cache.converters
 
+import androidx.room.ProvidedTypeConverter
 import androidx.room.TypeConverter
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import com.lnmcode.myweather.datasource.cache.entities.holder.WeatherItemEntityHolder
+import com.squareup.moshi.Moshi
 
-class WeatherConverter {
+@ProvidedTypeConverter
+class WeatherConverter(private val moshi: Moshi) {
 
     @TypeConverter
-    fun fromWeatherItemEntityHolder(holder: WeatherItemEntityHolder): String = Gson().toJson(holder)
+    fun fromWeatherItemEntityHolder(holder: WeatherItemEntityHolder): String =
+        moshi.adapter(WeatherItemEntityHolder::class.java).toJson(holder)
 
     @TypeConverter
-    fun fromStringWeatherItemEntityHolder(value: String): WeatherItemEntityHolder =
-        Gson().fromJson(value, object : TypeToken<WeatherItemEntityHolder>() {}.type)
+    fun fromStringWeatherItemEntityHolder(value: String): WeatherItemEntityHolder? =
+        moshi.adapter(WeatherItemEntityHolder::class.java).fromJson(value)
 }
