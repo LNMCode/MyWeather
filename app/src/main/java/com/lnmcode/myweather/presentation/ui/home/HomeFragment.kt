b08698550c94600b08d5e584dc364117844c64b8
LNMCode <longnmp31@gmail.com>
2022-06-06 18:49:51 +0700

    Add location permission

--- app/src/main/java/com/lnmcode/myweather/presentation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/lnmcode/myweather/presentation/ui/home/HomeFragment.kt
@@ -2,9 +2,13 @@ package com.lnmcode.myweather.presentation.ui.home
 
 import android.os.Bundle
 import android.view.View
+import com.google.android.material.snackbar.Snackbar
 import com.lnmcode.myweather.R
 import com.lnmcode.myweather.databinding.FragmentHomeBinding
 import com.lnmcode.myweather.presentation.base.BaseFragment
+import com.lnmcode.myweather.utils.PermissionUtils
+import com.lnmcode.myweather.utils.PermissionUtils.arePermissionGranted
+import com.lnmcode.myweather.utils.PermissionUtils.locationPermission
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
@@ -12,9 +16,17 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
     override val viewModel: HomeViewModel by viewModel()
 
+    private val dialogMultiplePermissionListener by lazy {
+        PermissionUtils.DialogMultiplePermissionListener(
+            view = requireView(),
+            onPermissionGranted = { onPermissionGranted() },
+            onPermissionDenied = { onPermissionDenied() }
+        )
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        configUIPermission()
     }
 
     override fun setUpLayout() {
@@ -22,15 +34,35 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     }
 
     override fun setUpEvents() {
-//        binding.ivBackground.setOnClickListener {
-//            Timber.d("Request")
-//            viewModel.getWeather()
-//        }
     }
 
     private fun setUpSlideViewPager() {
         val slideAdapter = HomeSlidePagerAdapter(this)
         binding.vpHome.adapter = slideAdapter
+    }
+
+    private fun configUIPermission() {
+        if (arePermissionGranted(requireContext(), locationPermission)) {
+            //
+        } else {
+            requestPermission()
+        }
+    }
+
+    private fun requestPermission() {
+        PermissionUtils.requestPermissions(
+            requireContext(),
+            dialogMultiplePermissionListener,
+            locationPermission
+        )
+    }
+
+    private fun onPermissionGranted() {
+
+    }
+
+    private fun onPermissionDenied() {
+        Snackbar.make(requireView(), "Permission denied", Snackbar.LENGTH_LONG).show()
     }
 
 }
